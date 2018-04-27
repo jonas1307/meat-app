6d7802edba93b99f2f266377b3bf9a0799725ed1
Jonas Lima <jonas1307@live.com>
2018-04-26 23:51:22 -0300

    Handling Errors

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import { Http } from "@angular/http"
 import { MEAT_API } from "../app.api";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import { ErrorHandler } from "../app.errorhandler";
 
 @Injectable()
 export class RestaurantService {
@@ -13,5 +15,6 @@ export class RestaurantService {
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 }
