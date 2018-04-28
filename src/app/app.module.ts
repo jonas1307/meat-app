01c47738bf70629327e8042e5d6fdb1f4053f10a
Jonas Lima <jonas1307@live.com>
2018-04-27 23:54:14 -0300

    Adding pt-BR localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -40,8 +40,9 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
-    RestaurantService, 
-    ShoppingCartService
+    RestaurantService,
+    ShoppingCartService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
 })
