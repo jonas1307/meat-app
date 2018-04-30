1c62ff5b5f132a657440da6e20f3df7d6727d312
Jonas Lima <jonas1307@live.com>
2018-04-29 23:29:00 -0300

    checking order and persisting

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from "@angular/core";
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "../restaurant-detail/shopping-cart/shopping-cart-item.model";
+import { Order } from "./order.model";
+import { Observable } from "rxjs/observable";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { MEAT_API } from "../app.api";
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService) { }
+  constructor(private cartService: ShoppingCartService, private http: Http) { }
 
   itemsValue(): number {
     return this, this.cartService.total()
@@ -25,4 +29,19 @@ export class OrderService {
   remove(item: CartItem) {
     this.cartService.removeItem(item)
   }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+
+    return this.http.post(`${MEAT_API}/orders`,
+      JSON.stringify(order),
+      new RequestOptions({ headers: headers }))
+      .map(response => response.json())
+  }
+
+  clear() {
+    this.cartService.clear()
+  }
+
 }
