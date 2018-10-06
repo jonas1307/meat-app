446167a04e108638ce3a0d059e98612c3cc2e838
Jonas Lima <jonas1307@live.com>
2018-10-06 20:26:23 -0300

    Reordering imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module'
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { ROUTES } from './app.routes'
 import { SharedModule } from './shared/shared.module'
-import { CoreModule } from './core/core.module'
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
