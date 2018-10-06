9841d8f9b28f76f24ccbbf362794f67993199013
Jonas Lima <jonas1307@live.com>
2018-10-06 14:44:51 -0300

    Redirecting navigation to order-summary after purchase

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/shopping-cart-item.model';
@@ -22,7 +23,7 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão Refeição', value: 'REF' }
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -48,7 +49,7 @@ export class OrderComponent implements OnInit {
       .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
 
     this.orderService.checkOrder(order).subscribe((orderId: string) => {
-      console.log(`Compra concluída: ${orderId}`)
+      this.router.navigate(['/order-summary'])
       this.orderService.clear()
     })
 
