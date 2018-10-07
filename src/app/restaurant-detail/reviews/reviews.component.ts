3802ebfe3a659ea650366ebf69b39ff1f7ab3927
Jonas Lima <jonas1307@live.com>
2018-10-07 15:28:09 -0300

    Adding animation to Reviews

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,3 +1,4 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { RestaurantService } from '../../restaurants/restaurants.service';
 import { Observable } from "rxjs/Observable";
@@ -5,9 +6,21 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-reviews',
-  templateUrl: './reviews.component.html'
+  templateUrl: './reviews.component.html',
+  animations: [
+    trigger('reviewsAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateX(-20px)' }),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
+
 export class ReviewsComponent implements OnInit {
+
+  reviewsState = 'ready'
 
   reviews: Observable<any>
 
