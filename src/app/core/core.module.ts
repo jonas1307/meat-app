7e89117aa3d381d0c592f801566a9133c34f4f3c
Jonas Lima <jonas1307@live.com>
2018-10-07 02:17:57 -0300

    Adding notifications

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core'
 import { OrderService } from '../order/order.service'
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service'
 import { RestaurantService } from '../restaurants/restaurants.service'
+import { NotificationService } from '../shared/messages/notification.service'
 
 @NgModule({
-  providers: [OrderService, ShoppingCartService, RestaurantService]
+  providers: [OrderService, ShoppingCartService, RestaurantService, NotificationService]
 })
 export class CoreModule { }
--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations'
+import { NotificationService } from '../notification.service'
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/observable/timer'
 
 @Component({
   selector: 'mt-snackbar',
@@ -18,9 +21,14 @@ export class SnackbarComponent implements OnInit {
   snackVisibility: string = 'hidden'
   message: string = 'Hello there!'
 
-  constructor() { }
+  constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.notificationService.notifier.subscribe(message => {
+      this.message = message
+      this.snackVisibility = 'visible'
+      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden')
+    })
   }
 
 }
