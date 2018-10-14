424076a7dc9444f27f7cb9faf722530155ab31f9
Jonas Lima <jonas1307@live.com>
2018-10-14 19:56:42 -0300

    Added validation on CheckOrder

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -3,12 +3,15 @@ import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping
 import { CartItem } from "../restaurant-detail/shopping-cart/shopping-cart-item.model";
 import { Order } from "./order.model";
 import { Observable } from "rxjs/observable";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { MEAT_API } from "../app.api";
+import { LoginService } from "app/security/login/login.service";
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService, private http: HttpClient) { }
+  constructor(private cartService: ShoppingCartService,
+    private http: HttpClient,
+    private loginService: LoginService) { }
 
   itemsValue(): number {
     return this, this.cartService.total()
@@ -31,8 +34,12 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
+    let headers = new HttpHeaders()
+    if (this.loginService.isLoggedIn) {
+      headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
+    }
 
-    return this.http.post<Order>(`${MEAT_API}/orders`, order)
+    return this.http.post<Order>(`${MEAT_API}/orders`, order, { headers: headers })
       .map(order => order.id)
   }
 
