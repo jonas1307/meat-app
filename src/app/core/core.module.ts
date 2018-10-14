253f5dc060595e3656ddd161f0910ce6d803f2e2
Jonas Lima <jonas1307@live.com>
2018-10-14 19:27:21 -0300

    Add LoginService

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,8 +3,9 @@ import { OrderService } from '../order/order.service'
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service'
 import { RestaurantService } from '../restaurants/restaurants.service'
 import { NotificationService } from '../shared/messages/notification.service'
+import { LoginService } from 'app/security/login/login.service';
 
 @NgModule({
-  providers: [OrderService, ShoppingCartService, RestaurantService, NotificationService]
+  providers: [LoginService, OrderService, ShoppingCartService, RestaurantService, NotificationService]
 })
 export class CoreModule { }
