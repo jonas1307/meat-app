ee99a43ce0448e97a972387cdac3c4a865ec8464
Jonas Lima <jonas1307@live.com>
2018-10-15 14:29:58 -0300

    Creating HttpInterceptor for Authorization

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core'
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { OrderService } from '../order/order.service'
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service'
 import { RestaurantService } from '../restaurants/restaurants.service'
@@ -6,8 +7,18 @@ import { NotificationService } from '../shared/messages/notification.service'
 import { LoginService } from 'app/security/login/login.service';
 import { LoginGuard } from 'app/security/login/login.guard';
 import { OrderGuard } from 'app/order/order.guard';
+import { AuthInterceptor } from 'app/security/auth.interceptor';
 
 @NgModule({
-  providers: [LoginGuard, LoginService, OrderGuard, OrderService, ShoppingCartService, RestaurantService, NotificationService]
+  providers: [
+    LoginGuard,
+    LoginService,
+    OrderGuard,
+    OrderService,
+    ShoppingCartService,
+    RestaurantService,
+    NotificationService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ]
 })
 export class CoreModule { }
